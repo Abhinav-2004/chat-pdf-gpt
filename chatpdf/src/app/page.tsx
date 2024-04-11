4387dd41e5ba00e4d15526ff1a32ee8f355a000f
Abhinav Singh <kittusinghranchi@gmail.com>
2024-04-11 15:50:36 +0530

    shadcn ui initialised and connected

--- chatpdf/src/app/page.tsx
+++ chatpdf/src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 export default function page() {
   return (
     <h1 className='text-red-500'>
-      Hello 
+      <Button>Click here</Button>
     </h1>
   )
 }
