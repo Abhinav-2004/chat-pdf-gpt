585e323d3279ea3d9756cc9e5e775d93c50e393c
Abhinav Singh <kittusinghranchi@gmail.com>
2024-04-16 17:50:50 +0530

    working

--- chatpdf/src/lib/context.ts
+++ chatpdf/src/lib/context.ts
@@ -32,7 +32,7 @@ export async function getContext(query:string, fileKey:string) {
     const matches=await getMatchesFromEmbeddings(queryEmbeddings, fileKey);
 
 
-    const qualifyingDocs=matches.filter(match=>match.score && match.score>0.7);
+    const qualifyingDocs=matches.filter(match=>match.score && match.score>0.5);
 
     type Metadata={
         text:string,
